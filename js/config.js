d61d495913e12d80a242ff6edbc09959add7891c
nam0713 <ax147@naver.com>
2023-01-12 05:04:02 +0900

    first commit

---
+++ js/config.js
@@ -0,0 +1,139 @@
+/*    _           _    _                     _ 
+     | |         | |  | |                   | |
+  ___| | ___  ___| | _| |     ___   __ _  __| |
+ / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
+ \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
+ |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
+    version 2.05
+*/
+
+/*URL for the server logo
+Leave it blank to use text instead*/
+var l_serverImage = "";
+
+/*Custom server name configuration
+Leave it blank for automatic server name*/
+var l_serverName = "";
+
+/*Use a video for the background?
+Otherwise it will just use images*/
+var l_bgVideo = false;
+
+/*Video background
+Avaiable options:
+"backgrounds/videos/aurora.webm"
+"backgrounds/videos/bluefall.webm"
+"backgrounds/videos/bluetilefloor.webm"
+"backgrounds/videos/bluewhitecircles.webm"
+"backgrounds/videos/bokehlens.webm"
+"backgrounds/videos/cleanbokeh.webm"
+"backgrounds/videos/coldnight.webm"
+"backgrounds/videos/colorfulburst.webm"
+"backgrounds/videos/dancingstars.webm"
+"backgrounds/videos/dropsflares.webm"
+"backgrounds/videos/focusorangebokeh.webm"
+"backgrounds/videos/glimmering.webm"
+"backgrounds/videos/glowingrings.webm"
+"backgrounds/videos/greencircles.webm"
+"backgrounds/videos/hues.webm"
+"backgrounds/videos/inthedeep.webm"
+"backgrounds/videos/modernflourish.webm"
+"backgrounds/videos/orangeaura.webm"
+"backgrounds/videos/warminside.webm"
+"backgrounds/videos/yellowgreen.webm"
+
+You can add custom backgrounds
+Just make sure it is at webm VP8 format */
+
+/*(Image-background only)
+Place one or more images
+Multiple images will automatically shuffle between them*/
+var l_bgImages = [
+	"backgrounds/images/1.png",
+	"backgrounds/images/2.png",
+	"backgrounds/images/3.png",
+	"backgrounds/images/4.png",
+	"backgrounds/images/5.png",
+	"backgrounds/images/6.png",
+	"backgrounds/images/7.png",
+	"backgrounds/images/8.png",
+	"backgrounds/images/9.png",
+	"backgrounds/images/10.png",
+	"backgrounds/images/11.png",
+	"backgrounds/images/12.png",
+	"backgrounds/images/13.png",
+	"backgrounds/images/14.png",
+	"backgrounds/images/15.png",
+	"backgrounds/images/16.png",
+	"backgrounds/images/17.png",
+	"backgrounds/images/18.png",
+	"backgrounds/images/19.png",
+	"backgrounds/images/20.png",
+	"backgrounds/images/21.png",
+	"backgrounds/images/22.png",
+	"backgrounds/images/23.png",
+	"backgrounds/images/24.png",
+	"backgrounds/images/25.png",
+	"backgrounds/images/26.png"
+];
+
+/*Random background images order?*/
+var l_bgImagesRandom = false;
+
+/*(Image-background only)
+Delay between background images changes
+in milliseconds*/
+var l_bgImageDuration = 10000;
+
+/*(Image-background only)
+Image background fade velocity
+in milliseconds*/
+var l_bgImageFadeVelocity = 500;
+
+/*Enable background overlay?*/
+var l_bgOverlay = false;
+
+/*Background darkening amount
+0 for none (0%), 100 for pitch black (100%)*/
+var l_bgDarkening = 0;
+
+/*Music player
+'true' or 'false' to enable music in the background*/
+var l_music = true;
+
+/*Display actual song's name?*/
+var l_musicDisplay = false;
+
+/*Music playlist
+Place how much Youtube IDs/.ogg you want*/
+var l_musicPlaylist = [
+	{ogg: "songs/metroost.ogg", name: "Lullaby of Deserted Hell"}
+	];
+
+/*Random music order?*/
+var l_musicRandom = true;
+
+/*Music volume
+Choose a value between 0 and 100*/
+var l_musicVolume = 25;
+
+/*Enable custom messages?*/
+var l_messagesEnabled = false;
+
+/*Enter your custom messages below*/
+var l_messages = [
+"Enjoy the server",
+"Thanks for joining us!",
+"Did you know these backgrounds and songs are from Touhou?"
+];
+
+/*Random message order?*/
+var l_messagesRandom = false;
+
+/*Delay between message changes
+in milliseconds*/
+var l_messagesDelay = 5000;
+
+/*Messages fade time
+in milliseconds*/
+var l_messagesFade = 1000;
